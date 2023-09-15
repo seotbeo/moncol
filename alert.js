ea3272e856529f3cd7190194477c513a4f69d667
seotbeo <yoonv72@naver.com>
2023-09-16 01:40:46 +0900

    Update alert.js

--- alert.js
+++ alert.js
@@ -3,9 +3,9 @@ function showAlert(t)
     const $alert = document.querySelector(".alertarea_in");
     $alert.innerHTML = "<div>" + t + "</div>";
 
-    $(".alertarea_in").finish().fadeTo();
-    
-    $(".alertarea_in").fadeTo(0, 1);
-    $(".alertarea_in").fadeTo(100, 1);
-    $(".alertarea_in").fadeTo(900, 0);
+    $(".alertarea_in").finish();
+
+    $(".alertarea_in").animate({opacity: '1'}, 0);
+    $(".alertarea_in").animate({opacity: '1'}, 100);
+    $(".alertarea_in").animate({opacity: '0'}, 900);
 }
