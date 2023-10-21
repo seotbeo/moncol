fdab508156647a8755497fabd0242310dcc5d9bc
seotbeo <yoonv72@naver.com>
2023-10-21 15:37:04 +0900

    Set font to Maplestory

--- moncol.js
+++ moncol.js
@@ -156,14 +156,13 @@ function drawMemo(memo)
     const canvas = document.getElementById("preview");
     const ctx = canvas.getContext("2d");
 
-    ctx.font = "17px SeoulNamsan"; //https://www.seoul.go.kr/seoul/font.do
+    ctx.font = "17px MaplestoryL";
     ctx.fillStyle = "#eeff00";
     ctx.textAlign = "right";
     ctx.letterSpacing = "1px";
     ctx.strokeStyle = "#eeff00";
 
-    ctx.fillText(memo, 635, 34, 525);
-    ctx.fillText(memo, 635, 33, 525);
+    ctx.fillText(memo, 635, 35, 525);
 }
 
 function addMobToList(mob)
